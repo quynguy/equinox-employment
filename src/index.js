2e82b1b52ff40534e9e86558fe71fc62f2b7256c
wquynguy <wquyenle@gmail.com>
2024-05-30 11:13:06 -0500

    added basic routes: home, about, job board, contact

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/about', (req, res) => {
+    res.send('About Page');
+  });
+  
+app.get('/contact', (req, res) => {
+    res.send('Contact Page');
+});
+
+app.get('/jobs', (req, res) => {
+    res.send('Job Board');
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
